848d01d22eb0e850c77b38b728e6c9b617a8955d
Obinneji Chibuzor Francis <Obinnejic@gmail.com>
2023-12-22 07:11:55 +0100

    settings modal except kyc modal

--- app/settings/fintech-services/wallet-functionality/page.jsx
+++ app/settings/fintech-services/wallet-functionality/page.jsx
@@ -1,22 +1,24 @@
-"use client"
-import {useState } from "react";
+"use client";
+import { useState } from "react";
 import ButtonUI from "@/components/ButtonUI";
 import Link from "next/link";
-import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import Modal from "@/components/CreateVendorModal";
-
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 export default function page() {
   const [open, setOpen] = useState(false);
-
   return (
     <main>
       <div className="flex items-center">
         <div className="flex flex-col">
-          <h2 className="text-2xl text-black font-bold">Wallet Functionality</h2>
+          <h2 className="text-2xl text-black font-bold">
+            Wallet Functionality
+          </h2>
           <p className="font-normal text-base text-[#0000006E]">
-          Define the features and limitations of user wallets
+            Define the features and limitations of user wallets
           </p>
         </div>
         <div className="mt-2 ml-auto flex items-center gap-4">
@@ -56,16 +58,15 @@ export default function page() {
       </div>
       {/* Body */}
       <div className="parent bg-white border-[1px] rounded-lg mt-6 px-4 py-6 ">
-
         <div className="bottom flex flex-row justify-between items-center mt-4 ">
           <div className="textContainer flex gap-4 ">
             <div className="iconContainer rounded-lg flex items-center bg-[#006FCF40] p-2 justify-center">
-              <AccountBalanceWalletIcon  style={{ fill: "#006FCF" }} />
+              <AccountBalanceWalletIcon style={{ fill: "#006FCF" }} />
             </div>
             <div>
               <h2 className="font-bold text-base">Wallet Creation</h2>
               <p className="font-normal text-sm text-[#00000099] ">
-              Enable users to create digital wallets for easy transactions
+                Enable users to create digital wallets for easy transactions
               </p>
             </div>
           </div>
@@ -77,46 +78,95 @@ export default function page() {
         <div className="top flex flex-row justify-between items-center ">
           <div className="textContainer flex gap-4 ">
             <div className="iconContainer rounded-lg flex items-center bg-[#006FCF40] p-2 justify-center">
-             <AccountBalanceWalletIcon  style={{ fill: "#006FCF" }} />
+              <AccountBalanceWalletIcon style={{ fill: "#006FCF" }} />
             </div>
             <div>
-              <h2 className="font-bold text-base">
-              Transaction Limits
-              </h2>
+              <h2 className="font-bold text-base">Transaction Limits</h2>
               <p className="font-normal text-sm text-[#00000099] ">
-              Configure transaction fees for different types of transactions
+                Configure transaction fees for different types of transactions
               </p>
             </div>
           </div>
           <div className="" onClick={() => setOpen(true)}>
-          <ArrowForwardIosIcon  />
-
+            <ArrowForwardIosIcon />
           </div>
         </div>
       </div>
       {/* Modal */}
       <Modal open={open} onClose={() => setOpen(false)}>
         <div className=" w-96">
-          <form action="">
-            <h2 className="text-black text-4xl mt-2 text-center">Create Vendor</h2>
-            <div className="flex flex-col gap-2 mt-2">
-              <label htmlFor="vendor" className="font-semibold text-xl">Vendor</label>
-              <input id="vendor" type="text" placeholder="Full Name" className=" border-2 p-2 rounded border-[#006FCF] bg-[#006FCF1F] outline-none"/>
-            </div> 
-            <div className="flex flex-col gap-2 mt-2">
-              <label htmlFor="phone"  className="font-semibold text-xl">Phone Number</label>
-              <input type="text" placeholder="Enter Phone Number" className=" border-2 p-2 rounded border-[#006FCF] bg-[#006FCF1F] outline-none" />
+          <form action="" className="flex flex-col">
+            <h2 className="text-center font-bold text-2xl">
+              Transaction Limit Setup
+            </h2>
+            <p className="text-center font-normal text-base">
+              This tool allows you to set parameters that align with your
+              business strategy while ensuring financial security and user
+              satisfaction
+            </p>
+            <div className="flex flex-col gap-2 ">
+              <label
+                htmlFor=""
+                className="font-bold text-base text-[#006FCF] mt-2"
+              >
+                Currency
+              </label>
+              <select
+                name=""
+                id=""
+                className=" rounded text-sm font-semibold text-[#006FCFD9] border-[#d1d1d1] border-2 outline-none bg-[#006FCF21] p-2"
+              >
+                <option required value="">
+                  Select Country
+                </option>
+              </select>
             </div>
-            <div className="flex flex-col gap-2 mt-2">
-              <label htmlFor="industry"  className="font-semibold text-xl">Industry</label>
-              <input type="text" placeholder="Select Your Industry" className=" border-2 p-2 rounded border-[#006FCF] bg-[#006FCF1F] outline-none"  />
+            <div className="flex flex-col gap-2">
+              <label
+                htmlFor=""
+                className="font-bold text-base text-[#006FCF] mt-2"
+              >
+                Set Transaction Limits
+              </label>
+              <div className="flex relative">
+                <input
+                  type="text"
+                  className="w-full rounded placeholder:text-sm placeholder:font-semibold placeholder:text-[#006FCFD9] border-[#006FCFD9] border-2 outline-none bg-[#006FCF21] p-2"
+                  placeholder="Limit amount"
+                />
+                <div className="flex absolute gap-6 bottom-0 right-0 top-[8px] right-[12px]  ">
+                  <div className="flex rounded-lg justify-center items-center bg-[#006FCF] w-[30px] h-[30px] ">
+                    <AddIcon style={{ fill: "#DEECF9" }} />
+                  </div>
+                  <div className="flex  rounded-lg justify-center items-center bg-[#006FCF] w-[30px] h-[30px] ">
+                    <RemoveIcon style={{ fill: "#DEECF9" }} />
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="flex flex-col gap-2 mt-2" >
-              <label htmlFor="services"  className="font-semibold text-xl">Vendor</label>
-              <input type="text" placeholder="Select Services" className=" border-2 p-2 rounded border-[#006FCF] bg-[#006FCF1F] outline-none" />
+            <div className="flex flex-col gap-2 ">
+              <label
+                htmlFor=""
+                className="font-bold text-base text-[#006FCF] mt-2 "
+              >
+                Fee Description
+              </label>
+              <textarea
+                col="10"
+                row="30"
+                className="rounded placeholder:text-sm placeholder:font-semibold placeholder:text-[#006FCFD9] border-[#006FCFD9] border-2 outline-none bg-[#006FCF21] p-2"
+                type="text"
+                placeholder="A brief description of the fee. This could be helpful for users who are not familiar with your services"
+              ></textarea>
             </div>
-            <button className=" my-4 w-full flex align-center justify-center text-center text-white bg-[#006FCF] font-semibold  rounded px-2 py-2">Add Vendor</button>
-            {/* <ButtonUI text="Add Vendor"></ButtonUI> */}
+            <div className="buttonConatiner text-center mx-auto  flex mt-4 ">
+              <button className=" flex align-center mr-6 justify-center text-center text-[#006FCF] bg-[#006FCF40] font-semibold  rounded px-4 py-2 ">
+                Cancel
+              </button>
+              <button className=" flex align-center justify-center text-center text-white bg-[#006FCF] font-semibold  rounded px-4 py-2 ">
+                Save
+              </button>
+            </div>
           </form>
         </div>
       </Modal>
