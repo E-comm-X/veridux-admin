daeaf2e7774c3df4adf69d338409427cfb2f5cdd
Osaretin Frank <osaretin.frank10@gmail.com>
2024-01-23 05:18:38 +0100

    change  add vendor form

--- app/(home)/vendors/page.tsx
+++ app/(home)/vendors/page.tsx
@@ -4,8 +4,11 @@ import ButtonUI from "@/components/ButtonUI"
 import FilterAltIcon from "@mui/icons-material/FilterAlt"
 import AddIcon from "@mui/icons-material/Add"
 import { useState } from "react"
-import Modal from "@/components/CreateVendorModal"
 import { VendorTable } from "./VendorTable"
+import { useGetAllStoresQuery } from "@/services/store.service"
+import { useAuthToken } from "@/hooks/useAuthToken"
+import { Form, Input, Select, message } from "antd"
+import { Modal } from "antd"
 
 export default function Page() {
   const [open, setOpen] = useState(false)
@@ -31,58 +34,46 @@ export default function Page() {
       <hr className="h-px mt-4 mb-4 bg-gray-200 border-0 " />
       <VendorTable />
       {/* Modal */}
-      <Modal open={open} onClose={() => setOpen(false)}>
-        <div className=" w-96">
-          <form action="">
+      <Modal open={open} onCancel={() => setOpen(false)} footer={false}>
+        <div className="">
+          <Form>
             <h2 className="text-black text-4xl mt-2 text-center">
               Create Vendor
             </h2>
             <div className="flex flex-col gap-2 mt-2">
               <label htmlFor="vendor" className="font-semibold text-xl">
-                Vendor
+                Name
               </label>
-              <input
+              <Input
+                size="large"
                 id="vendor"
                 type="text"
-                placeholder="Full Name"
-                className=" border-2 p-2 rounded border-[#006FCF] bg-[#006FCF1F] outline-none"
+                placeholder="Vendor Name"
               />
             </div>
             <div className="flex flex-col gap-2 mt-2">
               <label htmlFor="phone" className="font-semibold text-xl">
-                Phone Number
+                Description
               </label>
-              <input
-                type="text"
-                placeholder="Enter Phone Number"
-                className=" border-2 p-2 rounded border-[#006FCF] bg-[#006FCF1F] outline-none"
-              />
+              <Input size="large" type="text" placeholder="Enter Description" />
             </div>
             <div className="flex flex-col gap-2 mt-2">
               <label htmlFor="industry" className="font-semibold text-xl">
-                Industry
+                Address
               </label>
-              <input
-                type="text"
-                placeholder="Select Your Industry"
-                className=" border-2 p-2 rounded border-[#006FCF] bg-[#006FCF1F] outline-none"
-              />
+              <Input size="large" type="text" placeholder="Address" />
             </div>
             <div className="flex flex-col gap-2 mt-2">
               <label htmlFor="services" className="font-semibold text-xl">
-                Vendor
+                Category
               </label>
-              <input
-                type="text"
-                placeholder="Select Services"
-                className=" border-2 p-2 rounded border-[#006FCF] bg-[#006FCF1F] outline-none"
-              />
+              <Select placeholder={"Select Category"} size="large" />
             </div>
             <button className=" my-4 w-full flex align-center justify-center text-center text-white bg-[#006FCF] font-semibold  rounded px-2 py-2">
               Add Vendor
             </button>
             {/* <ButtonUI text="Add Vendor"></ButtonUI> */}
-          </form>
+          </Form>
         </div>
       </Modal>
     </main>
