522cc70675e76ab226f048de0375ec94917c7507
Osaretin Frank <osaretin.frank10@gmail.com>
2024-01-24 08:28:39 +0100

    add getDocuments Query

--- services/documents.service.ts
+++ services/documents.service.ts
@@ -3,6 +3,7 @@ import {
   CategoryI,
   CategoryRequestI,
 } from "@/interfaces/categories"
+import { DocumentRequestI } from "@/interfaces/documents"
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const documentsApi = createApi({
@@ -11,18 +12,42 @@ export const documentsApi = createApi({
     baseUrl: `${process.env.NEXT_PUBLIC_API_URI}/document`,
   }),
   endpoints: (builder) => ({
-    getAllCategories: builder.query<CategoryI[], null>({
-      query: () => {
+    getAllDocumentRequests: builder.query<
+      DocumentRequestI[],
+      { authToken: string }
+    >({
+      query: ({ authToken }) => {
         return {
-          url: "/get",
+          url: "/request",
           method: "GET",
+          headers: {
+            authorization: `Bearer ${authToken}`,
+          },
         }
       },
-      transformResponse: (data: CategoriesResponseI) =>
-        data.data.product_categories,
+      transformResponse: (data: {
+        data: { document_verification_request: DocumentRequestI[] }
+      }) => data.data.document_verification_request,
+    }),
+    getDocumentsByStatus: builder.query<
+      DocumentRequestI[],
+      { status: string; authToken: string }
+    >({
+      query: ({ authToken, status }) => {
+        return {
+          url: `/request/?status=${status}`,
+          method: "GET",
+          headers: {
+            authorization: `Bearer ${authToken}`,
+          },
+        }
+      },
+      transformResponse: (data: {
+        data: { document_verification_request: DocumentRequestI[] }
+      }) => data.data.document_verification_request,
     }),
     addCategory: builder.mutation<
-      CategoryI,
+      DocumentRequestI,
       { data: CategoryRequestI; authToken: string | null }
     >({
       query: ({ data, authToken }) => {
@@ -41,4 +66,5 @@ export const documentsApi = createApi({
   }),
 })
 
-export const { useGetAllCategoriesQuery, useAddCategoryMutation } = documentsApi
+export const { useGetAllDocumentRequestsQuery, useGetDocumentsByStatusQuery } =
+  documentsApi
