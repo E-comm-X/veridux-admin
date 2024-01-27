31577764206b17ebd8cdd7fd116c917910e9df0e
Osaretin Frank <osaretin.frank10@gmail.com>
2024-01-27 07:54:16 +0100

    add privilege page

--- app/(home)/privileges/page.tsx
+++ app/(home)/privileges/page.tsx
@@ -1,19 +1,17 @@
 "use client"
 import { H2 } from "@/components/Typography"
 import React from "react"
-import { PermissionGroups } from "./PermissionGroups"
-import { AddGroup } from "./AddGroup"
+import { AddPrivilege } from "../permissions/AddPrivilege"
 
 const Page = () => {
   return (
     <div>
       <div className="flex justify-between md:flex-row flex-col gap-3 md:items-center">
-        <H2>Permission Groups</H2>
+        <H2>Privileges</H2>
         <div className="flex gap-2">
-          <AddGroup />
+          <AddPrivilege />
         </div>
       </div>
-      <PermissionGroups />
     </div>
   )
 }
